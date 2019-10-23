1ab96dedd6e5a9327e43ad86e3e148b9a3b7aab6
Thai Son Vu <vuthaison198x@gmail.com>
2019-10-23 14:06:24 +0700

    tc1a

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import falcon
+import datetime
+
+now = datetime.datetime.now()
 
 #tc00
 class HealthResource:
@@ -14,10 +17,21 @@ class HelloResource:
     def on_get(self, req, resp, name):
         resp.body = f'Hello {name}'
 
+# tc1a
+class HiResource:
+    def on_get(self, req, resp):
+        if 0 <= int(now.hour) <= 11:
+            resp.body = f'Good morning'
+        elif 12 <= int(now.hour) <= 17:
+            resp.body = f'Good afternoon'
+        else:
+            resp.body = f'Good evening'
 
 app = falcon.API()
 app.add_route('/health', HealthResource())
 app.add_route('/hello/{name}', HelloResource())
+app.add_route('/hi', HiResource())
+
 # any other route should be placed before the handle_404 one
 app.add_sink(handle_404, '')
 
