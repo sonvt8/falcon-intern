60cc63f1b48d010a8ac0baac849cee6dfd4095ca
Nam G VU <namgivu@gmail.com>
2019-10-23 14:13:06 +0700

    review

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import falcon
 import datetime
 
-now = datetime.datetime.now()
+now = datetime.datetime.now()  #TODO khai bao ngay gan` code su dung; ko can phai toan cuc
 
 #tc00
 class HealthResource:
     def on_get(self, req, resp):
-        resp.body = 'Hello'
+        resp.body = 'Hello'  #TODO sai yeu cau, chi can tra ve 200, body rong~
 
-def handle_404(req, resp):
-    resp.status = falcon.HTTP_404
+def handle_404(req, resp):  #TODO khai bao gan .add_sink()
+    resp.status = falcon.HTTP_404  #TODO khi loi~ khong fai la 404 thi sao?
     resp.body = 'Not found'
 
 # tc1b
@@ -33,5 +33,5 @@ app.add_route('/hello/{name}', HelloResource())
 app.add_route('/hi', HiResource())
 
 # any other route should be placed before the handle_404 one
-app.add_sink(handle_404, '')
+app.add_sink(handle_404, '')  #TODO code nay tuyet voi!
 
