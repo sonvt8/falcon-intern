2d864baa84134352700722120fa9b04fed12bbf3
Thai Son Vu <vuthaison198x@gmail.com>
2019-10-23 12:57:13 +0700

    tc1b

--- main.py
+++ main.py
@@ -9,7 +9,15 @@ def handle_404(req, resp):
     resp.status = falcon.HTTP_404
     resp.body = 'Not found'
 
+# tc1b
+class HelloResource:
+    def on_get(self, req, resp, name):
+        resp.body = f'Hello {name}'
+
+
 app = falcon.API()
-app.add_route('/', HealthResource())
+app.add_route('/health', HealthResource())
+app.add_route('/hello/{name}', HelloResource())
 # any other route should be placed before the handle_404 one
 app.add_sink(handle_404, '')
+
