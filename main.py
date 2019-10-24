d9f416c1262c63f66b7b1601af292d725962fd7f
Thai Son Vu <vuthaison198x@gmail.com>
2019-10-24 12:19:31 +0700

    TODO resolved

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 import falcon
 import datetime
 
-now = datetime.datetime.now()  #TODO khai bao ngay gan` code su dung; ko can phai toan cuc
-
 #tc00
 class HealthResource:
     def on_get(self, req, resp):
-        resp.body = 'Hello'  #TODO sai yeu cau, chi can tra ve 200, body rong~
-
-def handle_404(req, resp):  #TODO khai bao gan .add_sink()
-    resp.status = falcon.HTTP_404  #TODO khi loi~ khong fai la 404 thi sao?
-    resp.body = 'Not found'
+        resp.body = ''
 
 # tc1b
 class HelloResource:
@@ -20,6 +14,7 @@ class HelloResource:
 # tc1a
 class HiResource:
     def on_get(self, req, resp):
+        now = datetime.datetime.now()
         if 0 <= int(now.hour) <= 11:
             resp.body = f'Good morning'
         elif 12 <= int(now.hour) <= 17:
@@ -27,11 +22,15 @@ class HiResource:
         else:
             resp.body = f'Good evening'
 
+
 app = falcon.API()
 app.add_route('/health', HealthResource())
 app.add_route('/hello/{name}', HelloResource())
 app.add_route('/hi', HiResource())
 
+def handle_404(req, resp):
+    # urllib.getcode()
+    resp.status = falcon.HTTP_CONFLICT #TODO khi loi~ khong fai la 404 thi sao? -->
+    resp.body = 'Not found'
 # any other route should be placed before the handle_404 one
-app.add_sink(handle_404, '')  #TODO code nay tuyet voi!
-
+app.add_sink(handle_404, '')
