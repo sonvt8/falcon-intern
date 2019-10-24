9e5c55803336819c4b3612a2a4008e9e92c479d7
Thai Son Vu <vuthaison198x@gmail.com>
2019-10-24 17:44:03 +0700

    test1c solved

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import falcon
 import datetime
+import re
 
-#tc00
+
+def verify(string):
+    # Make own character set and pass
+    # this as argument in compile method
+    regex = re.compile('[@_!#$%^&*()<>?/\|}{~:0-9]')
+
+    # Pass the string in search
+    # method of regex object.
+    if (regex.search(string) == None):
+        return True
+    else:
+        return False
+
+
+# tc00
 class HealthResource:
     def on_get(self, req, resp):
         resp.body = ''
 
-# tc1b
-class HelloResource:
-    def on_get(self, req, resp, name):
-        resp.body = f'Hello {name}'
 
 # tc1a
 class HiResource:
@@ -23,14 +34,34 @@ class HiResource:
             resp.body = f'Good evening'
 
 
+# tc1b
+class HelloResource:
+    def on_get(self, req, resp, name):
+        resp.body = f'Hello {name}'
+
+
+# tc1c
+class HolaResource:
+    def on_get(self, req, resp, name):
+        if name == '':
+            resp.body = f'name is required'
+        elif verify(name) is False:
+            resp.body = f'name must be a valid string'
+        else:
+            resp.body = f'Hola {name}'
+
+
 api = falcon.API()
 api.add_route('/health', HealthResource())
 api.add_route('/hello/{name}', HelloResource())
 api.add_route('/hi', HiResource())
+api.add_route('/hola/{name}', HolaResource())
+
 
 def handle_404(req, resp):
-    # urllib.getcode()
-    resp.status = falcon.HTTP_CONFLICT #TODO khi loi~ khong fai la 404 thi sao? -->
-    resp.body = 'Not found'
+    resp.status = falcon.HTTP_404
+    resp.body = 'Link not found'
+
+
 # any other route should be placed before the handle_404 one
 api.add_sink(handle_404, '')
