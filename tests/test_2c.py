2c0f2227754b076a7f3c9c13f6a88ad945f069f6
Engineer Intern <engineer-intern@gigacover.com>
2019-10-30 16:59:47 +0700

    trang aQA for 02c - update

--- tests/test_2c.py
+++ tests/test_2c.py
@@ -21,40 +21,47 @@ class Test(testing.TestCase):
         e = r.json.get('title'); # e aka exception
         assert 'all values cannot be empty' in e
 
-    def test_tc01(self):
-        r = self.simulate_post(f'/hello/', body=json.dumps({'fname':'abc', 'lname': ''}))
+    def test_tc0a(self):
+        r = self.simulate_post(f'/hello/', body=json.dumps({'fname': 'abc', 'lname': ''}))
         assert r.status_code != 200
         e = r.json.get('title'); # e aka exception
         assert 'all values cannot be empty' in e
 
 
-    def test_tc01a(self):
-        r = self.simulate_post(f'/hello/', body=json.dumps({'f_name':'123','l_name': '@#$'}))
+    def test_tc01(self):
+        r = self.simulate_post(f'/hello/', body=json.dumps({'fname': '123', 'lname': '@#$'}))
         assert r.status_code != 200
         e = r.json.get('title'); # e aka exception
         assert 'all values ​​must be valid' in e
 
+    def test_tc01a(self):
+        r = self.simulate_post(f'/hello/', body=json.dumps({'fname': '', 'lname': '@#$'}))
+        assert r.status_code != 200
+        e = r.json.get('title'); # e aka exception
+        assert 'all values ​​must be valid' in e
+
+
     def test_tc02(self):
-        r = self.simulate_post(f'/hello/', body=json.dumps({'':'abc','': 'abc'}))
+        r = self.simulate_post(f'/hello/', body=json.dumps({'': 'abc', '': 'abc'}))
         assert r.status_code != 200
         e = r.json.get('title'); # e aka exception
         assert 'all key cannot be empty' in e
 
     def test_tc02a(self):
-        r = self.simulate_post(f'/hello/', body=json.dumps({'fname':'abc','': 'abc'}))
+        r = self.simulate_post(f'/hello/', body=json.dumps({'fname': 'abc', '': 'abc'}))
         assert r.status_code != 200
         e = r.json.get('title'); # e aka exception
         assert 'all key cannot be empty' in e
 
 
     def test_tc03(self):
-        r = self.simulate_post(f'/hello/', body=json.dumps({'not_fname':'abc','not_lname': 'abc'}))
+        r = self.simulate_post(f'/hello/', body=json.dumps({'not_fname': 'abc', 'not_lname': 'abc'}))
         assert r.status_code != 200
         e = r.json.get('title'); # e aka exception
         assert 'key must be fname and lname' in e
 
     def test_tc03a(self):
-        r = self.simulate_post(f'/hello/', body=json.dumps({'fname':'abc','not_lname': 'abc'}))
+        r = self.simulate_post(f'/hello/', body=json.dumps({'fname': 'abc', 'not_lname': 'abc'}))
         assert r.status_code != 200
         e = r.json.get('title'); # e aka exception
         assert 'key must be fname and lname' in e
