e5c67828752385a5ea289c697e9abc87735e8aae
Engineer Intern <engineer-intern@gigacover.com>
2019-10-31 17:24:52 +0700

    trang aQA for 02c - last version

--- tests/test_2c.py
+++ tests/test_2c.py
@@ -16,52 +16,84 @@ class Test(testing.TestCase):
 
 
     def test_tc00(self):
-        r = self.simulate_post(f'/hello/', body=json.dumps({'fname':'', 'lname': ''}))
+        body = {
+            'fname': '',
+            'lname': ''
+        }
+        r = self.simulate_post(f'/hello/', body=json.dumps(body))
         assert r.status_code != 200
         e = r.json.get('title'); # e aka exception
         assert 'all values cannot be empty' in e
 
     def test_tc0a(self):
-        r = self.simulate_post(f'/hello/', body=json.dumps({'fname': 'abc', 'lname': ''}))
+        body = {
+            'fname': 'abc',
+            'lname': ''
+        }
+        r = self.simulate_post(f'/hello/', body=json.dumps(body))
         assert r.status_code != 200
         e = r.json.get('title'); # e aka exception
         assert 'all values cannot be empty' in e
 
 
     def test_tc01(self):
-        r = self.simulate_post(f'/hello/', body=json.dumps({'fname': '123', 'lname': '@#$'}))
+        body = {
+            'fname': '123',
+            'lname': '@#$'
+        }
+        r = self.simulate_post(f'/hello/', body=json.dumps(body))
         assert r.status_code != 200
         e = r.json.get('title'); # e aka exception
         assert 'all values ​​must be valid' in e
 
     def test_tc01a(self):
-        r = self.simulate_post(f'/hello/', body=json.dumps({'fname': '', 'lname': '@#$'}))
+        body = {
+            'fname': '',
+            'lname': '@#$'
+        }
+        r = self.simulate_post(f'/hello/', body=json.dumps(body))
         assert r.status_code != 200
         e = r.json.get('title'); # e aka exception
         assert 'all values ​​must be valid' in e
 
 
     def test_tc02(self):
-        r = self.simulate_post(f'/hello/', body=json.dumps({'': 'abc', '': 'abc'}))
+        body = {
+            '': 'abc',
+            '': 'abc'
+        }
+        r = self.simulate_post(f'/hello/', body=json.dumps(body))
         assert r.status_code != 200
         e = r.json.get('title'); # e aka exception
         assert 'all key cannot be empty' in e
 
     def test_tc02a(self):
-        r = self.simulate_post(f'/hello/', body=json.dumps({'fname': 'abc', '': 'abc'}))
+        body = {
+            'fname': 'abc',
+            '': 'abc'
+        }
+        r = self.simulate_post(f'/hello/', body=json.dumps(body))
         assert r.status_code != 200
         e = r.json.get('title'); # e aka exception
         assert 'all key cannot be empty' in e
 
 
     def test_tc03(self):
-        r = self.simulate_post(f'/hello/', body=json.dumps({'not_fname': 'abc', 'not_lname': 'abc'}))
+        body = {
+            'not_fname': 'abc',
+            'not_lname': 'abc'
+        }
+        r = self.simulate_post(f'/hello/', body=json.dumps(body))
         assert r.status_code != 200
         e = r.json.get('title'); # e aka exception
         assert 'key must be fname and lname' in e
 
     def test_tc03a(self):
-        r = self.simulate_post(f'/hello/', body=json.dumps({'fname': 'abc', 'not_lname': 'abc'}))
+        body = {
+            'fname': 'abc',
+            'not_lname': 'abc'
+        }
+        r = self.simulate_post(f'/hello/', body=json.dumps(body))
         assert r.status_code != 200
         e = r.json.get('title'); # e aka exception
         assert 'key must be fname and lname' in e
@@ -70,8 +102,11 @@ class Test(testing.TestCase):
     def test_tc04(self):
         first_name = 'some first name'
         last_name = 'some last name'
+        d = {
+            'message': f'Hello{first_name} {last_name}'
+            }
+        expected_out = json.dumps(d)
         r = self.simulate_post(f'/hello/', body=json.dumps({'fname': {first_name},'lname': {last_name}}))
-        expected_out = json.dumps({'message': f'hello {first_name} {last_name}'})
         assert r.status_code == 200
         assert r.text == expected_out
 
