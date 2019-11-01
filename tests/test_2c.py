d8619cf20fa6037fb31d25eda0bc14308684da30
Engineer Intern <engineer-intern@gigacover.com>
2019-11-01 12:22:25 +0700

    update 2c

--- tests/test_2c.py
+++ tests/test_2c.py
@@ -25,7 +25,7 @@ class Test(testing.TestCase):
         e = r.json.get('title'); # e aka exception
         assert 'all values cannot be empty' in e
 
-    def test_tc0a(self):
+    def test_tc01(self):
         body = {
             'fname': 'abc',
             'lname': ''
@@ -36,7 +36,7 @@ class Test(testing.TestCase):
         assert 'all values cannot be empty' in e
 
 
-    def test_tc01(self):
+    def test_tc02(self):
         body = {
             'fname': '123',
             'lname': '@#$'
@@ -46,7 +46,7 @@ class Test(testing.TestCase):
         e = r.json.get('title'); # e aka exception
         assert 'all values ​​must be valid' in e
 
-    def test_tc01a(self):
+    def test_tc03(self):
         body = {
             'fname': '',
             'lname': '@#$'
@@ -57,28 +57,7 @@ class Test(testing.TestCase):
         assert 'all values cannot be empty' in e
 
 
-    def test_tc02(self):
-        body = {
-            '': 'abc',
-            '': 'abc'
-        }
-        r = self.simulate_post(f'/hello/', body=json.dumps(body))
-        assert r.status_code != 200
-        e = r.json.get('title'); # e aka exception
-        assert 'all key cannot be empty' in e
-
-    def test_tc02a(self):
-        body = {
-            'fname': 'abc',
-            '': 'abc'
-        }
-        r = self.simulate_post(f'/hello/', body=json.dumps(body))
-        assert r.status_code != 200
-        e = r.json.get('title'); # e aka exception
-        assert 'all key cannot be empty' in e
-
-
-    def test_tc03(self):
+    def test_tc04(self):
         body = {
             'not_fname': 'abc',
             'not_lname': 'abc'
@@ -88,7 +67,7 @@ class Test(testing.TestCase):
         e = r.json.get('title'); # e aka exception
         assert 'key must be fname and lname' in e
 
-    def test_tc03a(self):
+    def test_tc05(self):
         body = {
             'fname': 'abc',
             'not_lname': 'abc'
@@ -99,12 +78,12 @@ class Test(testing.TestCase):
         assert 'key must be fname and lname' in e
 
 
-    def test_tc04(self):
+    def test_tc06(self):
         first_name = 'some first name'
         last_name = 'some last name'
         body = {
-            'fname': {first_name},
-            'lname': {last_name}
+            'fname': first_name,
+            'lname': last_name
         }
         d = {
             'message': f'Hello{first_name} {last_name}'
