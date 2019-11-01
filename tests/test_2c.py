4e7e18534e507c1c8a31d6a4f34c01873c67b0e1
Engineer Intern <engineer-intern@gigacover.com>
2019-11-01 12:27:16 +0700

    update unittest 2c

--- tests/test_2c.py
+++ tests/test_2c.py
@@ -56,38 +56,14 @@ class Test(testing.TestCase):
         e = r.json.get('title'); # e aka exception
         assert 'all values cannot be empty' in e
 
-
     def test_tc04(self):
-        body = {
-            'not_fname': 'abc',
-            'not_lname': 'abc'
-        }
-        r = self.simulate_post(f'/hello/', body=json.dumps(body))
-        assert r.status_code != 200
-        e = r.json.get('title'); # e aka exception
-        assert 'key must be fname and lname' in e
-
-    def test_tc05(self):
-        body = {
-            'fname': 'abc',
-            'not_lname': 'abc'
-        }
-        r = self.simulate_post(f'/hello/', body=json.dumps(body))
-        assert r.status_code != 200
-        e = r.json.get('title'); # e aka exception
-        assert 'key must be fname and lname' in e
-
-
-    def test_tc06(self):
         first_name = 'some first name'
         last_name = 'some last name'
         body = {
             'fname': first_name,
             'lname': last_name
         }
-        d = {
-            'message': f'Hello{first_name} {last_name}'
-            }
+        d = {'message': f'Hello{first_name} {last_name}'}
         expected_out = json.dumps(d)
         r = self.simulate_post(f'/hello/', body=json.dumps(body))
         assert r.status_code == 200
