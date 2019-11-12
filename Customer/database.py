1cc8a56bafda14762f747fbca6491c7c7dba335f
namgivu <namgivu@gmail.com>
2019-11-12 14:36:55 +0700

    db info from .env

--- Customer/database.py
+++ Customer/database.py
@@ -3,25 +3,15 @@ from sqlalchemy.orm import sessionmaker
 import os.path
 import sys
 
-# Load configuration file
-file_exists = os.path.isfile("config.py")
-if file_exists:
-    from config import USER, PASSWD, DB, HOST, PORT
-else:
-    with open("config.py", "a+") as f:
-        config_string = f'USER    = your user\n' \
-                        f'PASSWD  = your password\n' \
-                        f'HOST    = your IP\n' \
-                        f'PORT    = your port\n' \
-                        f'DB      = your database\n'
-        f.write(config_string)
-        print("File config.py not found. Please create one by copying from config.sample.py")
-        sys.exit(1)
-
+USER   = os.environ.get('USER')
+PASSWD = os.environ.get('PASSWD')
+DB     = os.environ.get('DB')
+HOST   = os.environ.get('HOST')
+PORT   = os.environ.get('PORT')
 
 class Database():
     # replace the user, password, hostname and database according to your configuration according to your information
-    url = 'postgresql://{user}:{passwd}@{host}:{port}/{db}'.format(
+    url ='postgresql://{user}:{passwd}@{host}:{port}/{db}'.format(
         user   = USER,
         passwd = PASSWD,
         host   = HOST,
