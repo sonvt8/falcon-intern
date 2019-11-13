6823cf4796f3fe9cb5220de0a1073a59e70ab0d5
Thai Son Vu <vuthaison198x@gmail.com>
2019-11-13 16:58:53 +0700

    fixed database auto create .env

--- Customer/database.py
+++ Customer/database.py
@@ -4,28 +4,28 @@ import sys
 import os
 from dotenv import load_dotenv; load_dotenv()  # auto load .env
 
-# file_exists = os.path.isfile(".env")
-# if file_exists:
-#     from  import USER, PASSWD, DB, HOST, PORT
-# else:
-#     with open(".env", "a+") as f:
-#         config_string = f'export USERNAME=yourUSER\n' \
-#                         f'export PASS=yourPASS\n' \
-#                         f'export DB=yourHOST\n' \
-#                         f'export HOST=yourHOST\n' \
-#                         f'export PORT=yourPORT\n'
-#         f.write(config_string)
-#         print("Please fill all configuration parameters in .evn located in your directory to connect database")
-#         sys.exit(1)
+file_exists = os.path.isfile("../.env")
+if file_exists:
+    pass
+else:
+    with open("../.env", "a+") as f:
+        config_string = f'export USERNAME=yourUSER\n' \
+                        f'export     PASS=yourPASS\n' \
+                        f'export       DB=yourHOST\n' \
+                        f'export     HOST=yourHOST\n' \
+                        f'export     PORT=yourPORT\n'
+        f.write(config_string)
+        print("Please fill all configuration parameters in .evn located in root directory to connect database")
+        sys.exit(1)
 
 class Database():
     # replace the user, password, hostname and database according to your configuration according to your informationdoc
     url = 'postgresql://{user}:{passwd}@{host}:{port}/{db}'.format(
-        user=os.getenv("USERNAME"),
-        passwd=os.getenv("PASS"),
-        host=os.getenv("HOST"),
-        port=os.getenv("PORT"),
-        db=os.getenv("DB")
+        user   =os.getenv("USERNAME"),
+        passwd =os.getenv("PASS"),
+        host   =os.getenv("HOST"),
+        port   =os.getenv("PORT"),
+        db     =os.getenv("DB")
     )
     engine = db.create_engine(url)
     session = sessionmaker(bind= engine)()
