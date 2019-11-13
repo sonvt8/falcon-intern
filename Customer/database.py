f74dbb4aa2220af56673fe1e46d76eccecfd29a1
Thai Son Vu <vuthaison198x@gmail.com>
2019-11-13 13:10:37 +0700

    set .env and env-sample

--- Customer/database.py
+++ Customer/database.py
@@ -4,19 +4,19 @@ import sys
 import os
 from dotenv import load_dotenv; load_dotenv()  # auto load .env
 
-file_exists = os.path.isfile(".env")
-if file_exists:
-    from config import USER, PASSWD, DB, HOST, PORT
-else:
-    with open(".env", "a+") as f:
-        config_string = f'export USERNAME=yourUSER\n' \
-                        f'export PASS=yourPASS\n' \
-                        f'export DB=yourHOST\n' \
-                        f'export HOST=yourHOST\n' \
-                        f'export PORT=yourPORT\n'
-        f.write(config_string)
-        print("Please fill all configuration parameters in .evn located in your directory to connect database")
-        sys.exit(1)
+# file_exists = os.path.isfile(".env")
+# if file_exists:
+#     from  import USER, PASSWD, DB, HOST, PORT
+# else:
+#     with open(".env", "a+") as f:
+#         config_string = f'export USERNAME=yourUSER\n' \
+#                         f'export PASS=yourPASS\n' \
+#                         f'export DB=yourHOST\n' \
+#                         f'export HOST=yourHOST\n' \
+#                         f'export PORT=yourPORT\n'
+#         f.write(config_string)
+#         print("Please fill all configuration parameters in .evn located in your directory to connect database")
+#         sys.exit(1)
 
 class Database():
     # replace the user, password, hostname and database according to your configuration according to your informationdoc
