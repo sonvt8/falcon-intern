e41a4178e20897590b956fe2494b38db9e997414
Thai Son Vu <vuthaison198x@gmail.com>
2019-11-13 12:34:58 +0700

    removed existed image and find working dir

--- bin/docker/docker-build.sh
+++ bin/docker/docker-build.sh
@@ -1,3 +1,14 @@
-docker build -t 'sonvt8/falcon_intern' .
+#!/usr/bin/env bash
+s=$BASH_SOURCE; s=$(dirname "$s"); s=$(cd "$s" && pwd); a="$s/../..";   a=$(cd "$a" && pwd); APP_HOME=$a
+
+# rm image
+docker image rm -f 'sonvt8/falcon_intern'
+
+# do build
+cd $APP_HOME
+    docker build -t 'sonvt8/falcon_intern' .
+cd - 1>/dev/null
+
+# aftermath check
 check_img=`docker image ls | grep -c sonvt8/falcon_intern`
 if [[ $check_img == 0 ]]; then echo 'image not found'; exit; fi
