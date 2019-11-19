7a167de7f5b6af1acdf5c863bb642e456b72a002
Thai Son Vu <vuthaison198x@gmail.com>
2019-11-20 00:22:47 +0700

    updated SQL automation

--- customers.sql
+++ customers.sql
@@ -1,22 +1,29 @@
---00 Create Databse
+--00 Find what activities are taking place
+SELECT * FROM pg_stat_activity WHERE datname = 'customer_api';
+
+--01 terminate the connection
+SELECT pg_terminate_backend (pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'customer_api';
+
+--03 Drop and Create Database
+DROP DATABASE customer_api;
 CREATE DATABASE customer_api;
 \c customer_api;
 
---01 Create Table
+--04 Create Table
 DROP TABLE IF EXISTS customers;
 CREATE TABLE customers(
-	id int PRIMARY KEY ,
-	name varchar(100) not null,
-	dob date not null,
-	updated_at timestamp
+    id          serial PRIMARY KEY,
+    name        varchar (50) NOT NULL,
+    dob         date,
+    updated_at  timestamp
 );
 
---02 Insert Customer Values
-INSERT INTO customers VALUES
+--05 Insert Customer Values
+INSERT INTO customers (name, dob, updated_at) VALUES
     ('Thai Son'    , '1988-05-15', '2019-11-07 23:31:40'),
     ('Thai Binh'   , '1993-07-13', '2019-11-07 23:31:40'),
     ('Minh Anh'    , '1982-01-01', '2019-11-07 23:31:40'),
-    ('Name04'      , '2011-12-01', '2019-11-07 23:31:40'),
-    ('Name05'      , '2018-05-06', '2019-08-16 08:09:01')
+    ('Thanh Nguyen', '1985-12-04', '2019-11-07 23:31:40'),
+    ('Duc Nguyen'  , '1997-10-03', '2019-11-07 23:31:40')
 
 
